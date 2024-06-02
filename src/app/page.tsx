e8d524c0c7c8c18d23151f91160588153b12eb6d
Murad Aghakishiyev <murad.aghakishiyev@limestonedigital.com>
2024-06-02 23:30:37 +0700

    title added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,8 @@ export default async function Home() {
         </section>
 
         <section id="projects">
+          <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+            <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2></div>
           <ol className="group/list">
             {projectList.map(project => (
               <li key={project.id}>
